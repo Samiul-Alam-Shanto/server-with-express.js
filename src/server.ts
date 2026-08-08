8537ad59568afe84d936ade6a8d9f62f6e82c35a
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-08-08 15:10:48 +0600

    created a table in the neon database and connected the database successfully

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,26 @@ const pool = new Pool({
   connectionString: `postgresql://neondb_owner:${process.env.CONNECTION_PASSWORD}@ep-shiny-grass-ayo4dz4e-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
 });
 
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(25) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )`);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+initDB();
+
 app.get("/", (req: Request, res: Response) => {
   //   res.send("Hello World!");
   res.status(200).json({
