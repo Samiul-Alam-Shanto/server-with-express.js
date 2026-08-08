8325c1038713918da18e759366c0d33b454d6de0
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-08-08 14:41:04 +0600

    connected the neondb connection string using the postgre Pool and used dotenv

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,18 @@ import express, {
   type Request,
   type Response,
 } from "express";
+import { Pool } from "pg";
+import "dotenv/config";
 const app: Application = express();
 const port = 5000;
 
 app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+  connectionString: `postgresql://neondb_owner:${process.env.CONNECTION_PASSWORD}@ep-shiny-grass-ayo4dz4e-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+});
 
 app.get("/", (req: Request, res: Response) => {
   //   res.send("Hello World!");
@@ -16,7 +24,12 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 app.post("/", async (req: Request, res: Response) => {
-  console.log(req.body);
+  //   console.log(req.body);
+  const { name, email, password } = req.body;
+  res.status(200).json({
+    message: "Created",
+    data: { name, email },
+  });
 });
 
 app.listen(port, () => {
