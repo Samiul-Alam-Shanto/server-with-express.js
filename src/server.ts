5dd4074d7ea747c7c5d9de1f723dc38123df8417
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-08-09 14:42:51 +0600

    fixed the bug for updating user for only  any values using COALESCE()

--- src/server.ts
+++ src/server.ts
@@ -125,12 +125,19 @@ app.put("/api/users/:id", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(
       `
-      UPDATE users SET name=$1,password=$2,age=$3,is_active=$4 WHERE id=$5 RETURNING *
+      UPDATE users 
+      SET 
+      name=COALESCE($1,name),
+      password=COALESCE($2,password),
+      age=COALESCE($3,age),
+      is_active=COALESCE($4,is_active) 
+
+      WHERE id=$5 RETURNING *
       `,
-      [name, password, age, is_active],
+      [name, password, age, is_active, id],
     );
     if (result.rows.length === 0) {
-      res.status(404).json({
+      return res.status(404).json({
         success: false,
         message: "User Not Found",
         data: {},
