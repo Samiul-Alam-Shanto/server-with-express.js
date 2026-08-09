63ef4904d51d24741edd728ff8fadcaa270b7229
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-08-09 14:51:38 +0600

    created the delete api using DELETE method and handled error for no user using rowcount

--- src/server.ts
+++ src/server.ts
@@ -157,6 +157,36 @@ app.put("/api/users/:id", async (req: Request, res: Response) => {
   }
 });
 
+app.delete("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(
+      `
+      DELETE FROM users WHERE id=$1
+      `,
+      [id],
+    );
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User Not Found",
+        data: {},
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "User Deleted successfully",
+      data: null,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
