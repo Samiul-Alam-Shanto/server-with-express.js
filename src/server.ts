3ee3df47bbf9e84cc5417d82e77fe2fcd9be9399
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-08-09 15:01:03 +0600

    set the env files using the config folder

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,16 @@ import express, {
   type Response,
 } from "express";
 import { Pool } from "pg";
-import "dotenv/config";
+import config from "./config";
 const app: Application = express();
-const port = 5000;
+const port = config.port;
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
-  connectionString: `postgresql://neondb_owner:${process.env.CONNECTION_PASSWORD}@ep-shiny-grass-ayo4dz4e-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+  connectionString: config.connection_string,
 });
 
 const initDB = async () => {
