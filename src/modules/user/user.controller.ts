c679c6c6195c5c4435891586ba8909753f0b192b
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-08-10 07:17:18 +0600

    implemented moduler pattern file structure for handling all features of users

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -22,4 +22,104 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
-export const userController = { createUser };
+const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const result = await userService.getAllUsersFromDB();
+    res.status(200).json({
+      success: true,
+      message: "User retrieves successfully",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+};
+
+const getSingleUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  // console.log(id);
+  try {
+    const result = await userService.getSingleUserFromDB(id as string);
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+        data: {},
+      });
+    }
+    // console.log(result.rows[0]);
+    res.status(200).json({
+      success: true,
+      message: "User retrieves successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+};
+
+const updateUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await userService.updateUserIntoDB(id as string, req.body);
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User Not Found",
+        data: {},
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await userService.deleteUserFromDB(id as string);
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User Not Found",
+        data: {},
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "User Deleted successfully",
+      data: null,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+};
+
+export const userController = {
+  createUser,
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+};
