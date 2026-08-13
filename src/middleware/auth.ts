30efc7dab70caa0744c6e5c1a7c7f6581174cf9a
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-08-13 16:37:20 +0600

    done some validation for the all users auth

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import { pool } from "../db";
 
 const auth = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -9,9 +12,37 @@ const auth = () => {
         success: false,
         message: "Unauthorized Access !!",
       });
-    } else {
-      next();
     }
+
+    const decodedToken = jwt.verify(
+      token as string,
+      config.secret as string,
+    ) as JwtPayload;
+    // console.log(decodedToken);
+
+    const userData = await pool.query(
+      `
+        
+        SELECT * FROM users WHERE email=$1`,
+      [decodedToken.email],
+    );
+    // console.log(userData);
+    const user = userData.rows[0];
+    // console.log(user);
+    if (userData.rows[0].length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+      });
+    }
+    if (!user.is_active) {
+      res.status(403).json({
+        success: false,
+        message: "Forbidden",
+      });
+    }
+
+    next();
   };
 };
 
