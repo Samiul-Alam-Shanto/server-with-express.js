4bcfe34ea4798a8697a92552d86b29638b4ffd4f
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-08-13 16:11:09 +0600

    added the auth middleware initially

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,14 +1,11 @@
 import { Router, type NextFunction } from "express";
 import { userController } from "./user.controller";
+import auth from "../../middleware/auth";
 
 const router = Router();
 
-const auth = (req: Request, res: Response, next: NextFunction) => {
-  console.log("This is protected route");
-};
-
 router.post("/", userController.createUser);
-router.get("/", auth, userController.getAllUsers);
+router.get("/", auth(), userController.getAllUsers);
 router.get("/:id", userController.getSingleUser);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
