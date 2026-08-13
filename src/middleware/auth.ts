0a4b26eb76bea64b08b9e4354497450adeea2220
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-08-13 17:34:17 +0600

    added some more validation and also added typescript namespace for handing req.user

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,44 +5,49 @@ import { pool } from "../db";
 
 const auth = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    // console.log(req.headers.authorization);
-    const token = req.headers.authorization;
-    if (!token) {
-      res.status(401).json({
-        success: false,
-        message: "Unauthorized Access !!",
-      });
-    }
+    try {
+      // console.log(req.headers.authorization);
+      const token = req.headers.authorization;
+      if (!token) {
+        res.status(401).json({
+          success: false,
+          message: "Unauthorized Access !!",
+        });
+      }
 
-    const decodedToken = jwt.verify(
-      token as string,
-      config.secret as string,
-    ) as JwtPayload;
-    // console.log(decodedToken);
+      const decodedToken = jwt.verify(
+        token as string,
+        config.secret as string,
+      ) as JwtPayload;
+      // console.log(decodedToken);
 
-    const userData = await pool.query(
-      `
+      const userData = await pool.query(
+        `
         
         SELECT * FROM users WHERE email=$1`,
-      [decodedToken.email],
-    );
-    // console.log(userData);
-    const user = userData.rows[0];
-    // console.log(user);
-    if (userData.rows[0].length === 0) {
-      res.status(404).json({
-        success: false,
-        message: "User Not Found",
-      });
-    }
-    if (!user.is_active) {
-      res.status(403).json({
-        success: false,
-        message: "Forbidden",
-      });
-    }
+        [decodedToken.email],
+      );
+      // console.log(userData);
+      const user = userData.rows[0];
+      // console.log(user);
+      if (userData.rows[0].length === 0) {
+        res.status(404).json({
+          success: false,
+          message: "User Not Found",
+        });
+      }
+      if (!user.is_active) {
+        res.status(403).json({
+          success: false,
+          message: "Forbidden",
+        });
+      }
+      req.user = decodedToken;
 
-    next();
+      next();
+    } catch (error) {
+      next(error);
+    }
   };
 };
 
