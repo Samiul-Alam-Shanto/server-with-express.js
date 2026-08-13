f2b5b927619014a09ea73de0d1f9df80466aaec3
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-08-13 16:00:54 +0600

    working on adding logger and auth middleware with modular pattern

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import express, {
   type Application,
+  type NextFunction,
   type Request,
   type Response,
 } from "express";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
+import logger from "./middleware/logger";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(logger);
 
 app.get("/", (req: Request, res: Response) => {
   //   res.send("Hello World!");
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,10 +1,14 @@
-import { Router } from "express";
+import { Router, type NextFunction } from "express";
 import { userController } from "./user.controller";
 
 const router = Router();
 
+const auth = (req: Request, res: Response, next: NextFunction) => {
+  console.log("This is protected route");
+};
+
 router.post("/", userController.createUser);
-router.get("/", userController.getAllUsers);
+router.get("/", auth, userController.getAllUsers);
 router.get("/:id", userController.getSingleUser);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
